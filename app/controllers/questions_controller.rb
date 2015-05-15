de14f9b007311ad001504c6b803835589e633dd1
YOUR NAME <erinpagemd@yahoo.com>
2015-05-15 11:06:19 -0500

    setting up to create questions in the database

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,6 +2,12 @@ require "highline/import"
 
 class QuestionsController
   def index
+    # return all the questions currenty in the database
     "No questions found.\n"
   end
+
+  def add(name)
+    # add a new question?? 
+  end
+
 end
---
+++ lib/data_store.rb
@@ -0,0 +1,14 @@
+require 'sqlite3'
+
+class DataStore
+
+  def self.load_structure
+  end
+
+  def self.execute
+  end
+
+  def self.initialize_database
+  end
+
+end
